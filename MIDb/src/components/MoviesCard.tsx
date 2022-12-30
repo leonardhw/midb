2ff64798f2c58be300a57671b40e557d8cd9aa04
leonardhw <leonardharyow@gmail.com>
2022-12-30 22:04:57 +0700

    Styling on movies list screen and bottom navbar done

--- MIDb/src/components/MoviesCard.tsx
+++ MIDb/src/components/MoviesCard.tsx
@@ -1,9 +1,12 @@
-import { StyleSheet, ListRenderItemInfo } from "react-native";
+import { StyleSheet, ListRenderItemInfo, Dimensions } from "react-native";
 import React from "react";
 import { HStack, Image, Pressable, Text, View, VStack } from "native-base";
 import { useNavigation } from "@react-navigation/native";
 import { RootNavigationProps, RootStackParamList, States } from "../../types";
 import { NativeStackNavigationProp } from "@react-navigation/native-stack";
+import { AntDesign } from "@expo/vector-icons";
+
+const { width, height } = Dimensions.get("screen");
 
 interface Props {
   item: States["movie"];
@@ -12,13 +15,31 @@ interface Props {
 const MoviesCard = ({ item }: Props) => {
   const navigation = useNavigation<NativeStackNavigationProp<RootStackParamList>>();
 
+  const getDate = (date: any) => {
+    const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
+    let [year, month, day] = date.split("-");
+
+    return `${months[Number(month) - 1]} ${day}, ${year}`;
+  };
+
   return (
     <Pressable onPress={() => navigation.push("MovieDetail", { id: item.id })}>
-      <View>
-        <Image source={{ uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}` }} height={100} width={100} alt="Test" />
-        <Text>{item.id}</Text>
-        <Text>{item.title}</Text>
-        <Text>{item.vote_average}</Text>
+      <View style={styles.container}>
+        <View style={styles.imageContainer}>
+          <Image source={{ uri: `https://image.tmdb.org/t/p/w500/${item.poster_path}` }} alt={item.title} style={styles.image} />
+        </View>
+
+        <HStack style={styles.ratingContainer}>
+          <AntDesign name="star" size={12} color="#f5b754" />
+          <Text style={styles.rating}>{item.vote_average}</Text>
+        </HStack>
+
+        <View>
+          <View style={styles.titleContainer}>
+            <Text style={styles.title}>{item.title}</Text>
+          </View>
+          <Text style={styles.subTitle}>{getDate(item.release_date?.toString())}</Text>
+        </View>
       </View>
     </Pressable>
   );
@@ -26,4 +47,54 @@ const MoviesCard = ({ item }: Props) => {
 
 export default MoviesCard;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    // flex: 1 / 2,
+    marginVertical: 15,
+    flexWrap: "wrap",
+    width: 500 / 3,
+    // padding: 5,
+    // alignItems: "center",
+    // justifyContent: "center",
+  },
+  imageContainer: {
+    flex: 1,
+  },
+  image: {
+    borderRadius: 15,
+    width: 500 / 3,
+    height: 750 / 3,
+  },
+  ratingContainer: {
+    position: "absolute",
+    zIndex: 3,
+    top: 0,
+    right: 0,
+    backgroundColor: "#181818",
+    padding: 10,
+    borderTopEndRadius: 15,
+    borderBottomStartRadius: 15,
+    alignItems: "center",
+  },
+  rating: {
+    fontSize: 16,
+    fontWeight: "bold",
+    color: "#f5b754",
+    marginLeft: 5,
+  },
+  titleContainer: {
+    flexDirection: "row",
+    flexWrap: "wrap",
+  },
+  title: {
+    fontWeight: "700",
+    fontSize: 16,
+    color: "#f3f3f3",
+    marginTop: 20,
+  },
+  subTitle: {
+    fontWeight: "700",
+    fontSize: 12,
+    color: "#878787",
+  },
+});
--- MIDb/src/screens/MoviesListScreen.tsx
+++ MIDb/src/screens/MoviesListScreen.tsx
@@ -1,10 +1,12 @@
-import { StyleSheet, Text, View, ListRenderItem, FlatList } from "react-native";
+import { StyleSheet, ListRenderItem, Dimensions, StatusBar } from "react-native";
 import React, { useEffect, useState } from "react";
 import { RootTabScreenProps, States } from "../../types";
-import { Button } from "native-base";
+import { Button, Text, View, FlatList, HStack } from "native-base";
 import MoviesCard from "../components/MoviesCard";
 import axios from "../apis/axios";
 import { SafeAreaView } from "react-native-safe-area-context";
+
+const { width, height } = Dimensions.get("screen");
 
 const MoviesListScreen = ({ navigation, route }: RootTabScreenProps<"MoviesList">) => {
   const [movies, setMovies] = useState<States["movie"][]>([]);
@@ -27,21 +29,66 @@ const MoviesListScreen = ({ navigation, route }: RootTabScreenProps<"MoviesList"
     }
   };
   const headerSection = () => (
-    <>
-      <View>
-        <Text>Movie List Screen</Text>
-      </View>
-    </>
+    <View style={styles.header}>
+      <Text style={styles.title}>DISCOVER</Text>
+      <HStack style={styles.navigation}>
+        <Text style={[styles.navLink, styles.navLink_active]}>Now Playing</Text>
+        <Text style={styles.navLink}>Popular</Text>
+        <Text style={styles.navLink}>Upcoming</Text>
+      </HStack>
+    </View>
   );
 
   return (
     <SafeAreaView>
-      <FlatList data={movies} renderItem={({ item }) => <MoviesCard item={item} />} keyExtractor={(item: any) => item.id} />
+      <StatusBar barStyle="light-content" backgroundColor="#030303"></StatusBar>
+      <View style={styles.container}>
+        <FlatList data={movies} renderItem={({ item }) => <MoviesCard item={item} />} keyExtractor={(item: any) => item.id} ListHeaderComponent={headerSection} numColumns={2} columnWrapperStyle={styles.row} contentContainerStyle={styles.listContainer} />
+      </View>
     </SafeAreaView>
+
     // <Button onPress={() => navigation.navigate("Home")}>Test</Button>
   );
 };
 
 export default MoviesListScreen;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    backgroundColor: "#030303",
+    // paddingBottom: 70,
+  },
+  row: {
+    flex: 1,
+    paddingHorizontal: 20,
+    justifyContent: "space-between",
+  },
+  listContainer: { paddingBottom: 70 },
+  header: {
+    // marginVertical: 10,
+    paddingHorizontal: 20,
+    // backgroundColor: "tomato",
+    backgroundColor: "#030303",
+  },
+  title: {
+    fontSize: 28,
+    fontWeight: "bold",
+    lineHeight: 30,
+    marginTop: 20,
+    marginBottom: 20,
+    color: "#f3f3f3",
+  },
+  navigation: {
+    // marginRight: ,
+  },
+  navLink: {
+    fontSize: 16,
+    fontWeight: "700",
+    marginBottom: 10,
+    marginEnd: 15,
+    color: "#878787",
+  },
+  navLink_active: {
+    color: "#f3f3f3",
+  },
+});
